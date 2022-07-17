41e481d5450b4fb03e81fd6a5efa5cda2b3a279e
aleksandree128 <aleksandree128@mail.ru>
2022-07-17 17:59:12 +0500

    mesto-server-15

--- src/utils/const.js
+++
@@ -1,7 +0,0 @@
-export const apiUrl = {
-    baseUrl: `${window.location.protocol}${process.env.REACT_APP_API_URL || '//localhost:3001'}`,
-    headers: {
-        "Accept": "application/json",
-        "Content-Type": "application/json",
-    }
-}
